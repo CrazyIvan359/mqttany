d22190a340e51cd47663a10d357b6645240e390c
Michael Murton <6764025+CrazyIvan359@users.noreply.github.com>
2019-09-22 11:15:14 -0400

    remove changes to modules/__init__.py

--- mqttany/modules/__init__.py
+++ mqttany/modules/__init__.py
@@ -61,8 +61,7 @@ def load(config_file):
 
         except ImportError as err:
             log.error("Failed to import module '{name}'".format(name=module_name))
-            #log.error("  {error}".format(error=err))
-            log_traceback(log)
+            log.error("  {error}".format(error=err))
             log.error("Module '{name}' was not loaded".format(name=module_name))
 
         except ImportWarning as err:
