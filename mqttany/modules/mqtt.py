b26ca9776b6e1c70b5b829bae04bce408bb46437
Michael Murton <6764025+CrazyIvan359@users.noreply.github.com>
2019-09-22 10:46:25 -0400

    fix wrong MQTTMessage proprerty

--- mqttany/modules/mqtt.py
+++ mqttany/modules/mqtt.py
@@ -448,4 +448,4 @@ def on_message(client, userdata, message):
     Gets called when a message is received on a topic without a specific listener
     """
     log.debug("Received message without specific listener: [topic: '{topic}', message: '{message}']".format(
-            topic=message.topic, message=message.message))
+            topic=message.topic, message=message.payload))
