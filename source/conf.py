eb0af1ae0d374fde9d4be140cd036e78cf5aee05
Michael Murton <6764025+CrazyIvan359@users.noreply.github.com>
2021-01-20 20:57:29 -0500

    bump to dev

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ copyright = "2021, MQTTany Contributors"
 author = "Michael Murton"
 
 # The full version, including alpha/beta/rc tags
-release = "v0.13.0"
+release = "v0.13.0-dev"
 version = release
 
 # Provide global replace directives
